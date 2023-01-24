039cbc2ab11fa6c40591061899ca252cabb059c0
AndrejTutka <tutkaandrejko@gmail.com>
2023-01-24 09:11:20 +0100

    Update 2cisla.py

--- Number comparison/2cisla.py
+++ Number comparison/2cisla.py
@@ -1,9 +1,15 @@
-a = input('enter a number:  ')
-b = input('enter a another number:  ')
+try:
 
-if a > b:
-    print(f'greater number is {a}')
-if b > a:
-    print(f'greater number is {b}')
-if a ==b:
-    print('they are the same number')
+    a = input('enter a number:  ')
+    b = input('enter a another number:  ')
+
+
+    if a > b:
+        print(f'greater number is {a}')
+    if b > a:
+        print(f'greater number is {b}')
+    if a ==b:
+        print('they are the same number')
+
+except ValueError:
+        print('Both numbers are not intigers')
