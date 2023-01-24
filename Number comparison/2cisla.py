951b99c0b71afe82d9e77250076cdefcfa0c449f
AndrejTutka <tutkaandrejko@gmail.com>
2023-01-24 09:19:54 +0100

    Update 2cisla.py

--- Number comparison/2cisla.py
+++ Number comparison/2cisla.py
@@ -1,14 +1,14 @@
 try:
 
-    a = input('enter a number:  ')
-    b = input('enter a another number:  ')
+    a = int(input('enter a number:  '))
+    b = int(input('enter a another number:  '))
 
 
     if a > b:
         print(f'greater number is {a}')
-    if b > a:
+    elif b > a:
         print(f'greater number is {b}')
-    if a ==b:
+    elif a == b:
         print('they are the same number')
 
 except ValueError:
