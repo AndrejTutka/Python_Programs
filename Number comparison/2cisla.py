5c45b24c527aa1fc1e165d42fb8047e4d1ff8c7c
AndrejTutka <tutkaandrejko@gmail.com>
2023-01-24 08:46:36 +0100

    Create 2cisla.py
    2 cisla ktore vacsie

---
+++ Number comparison/2cisla.py
@@ -0,0 +1,9 @@
+a = input('enter a number:  ')
+b = input('enter a another number:  ')
+
+if a > b:
+    print(f'greater number is {a}')
+if b > a:
+    print(f'greater number is {b}')
+if a ==b:
+    print('they are the same number')
