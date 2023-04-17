731f2049befb64f7dc42652ffa2fb1dbe8307d77
AndrejTutka <tutkaandrejko@gmail.com>
2023-04-17 14:07:47 +0200

    Update date and time.py

--- List/date and time.py
+++ List/date and time.py
@@ -12,23 +12,29 @@
 # now2 = datetime.datetime.now()
 # print(now2 - right_now)
 
+# 
+# import datetime
+# 
+# right_now = datetime.datetime.now()
+# print(right_now)
+# 
+# weekdays = ['monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday']
+# 
+# print(f'Day: {right_now.day}')
+# print(f'Month: {right_now.month}')
+# print(f'Year: {right_now.year}')
+# print(f'Hour: {right_now.hour}')
+# print(f'Minute: {right_now.minute}')
+# print(f'Second: {right_now.second}')
+# print(f'Microsecond: {right_now.microsecond}')
+# print(f'Weekday: {right_now.weekday()}')
+
 
 import datetime
 
 right_now = datetime.datetime.now()
 print(right_now)
-
-weekdays = ['monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday']
-
-print(f'Day: {right_now.day}')
-print(f'Month: {right_now.month}')
-print(f'Year: {right_now.year}')
-print(f'Hour: {right_now.hour}')
-print(f'Minute: {right_now.minute}')
-print(f'Second: {right_now.second}')
-print(f'Microsecond: {right_now.microsecond}')
-print(f'Weekdays: {right_now.weekday()}')
-
-
-
+year_start = datetime.datetime.strptime('2005-06-11', '%Y-%m-%d')
+print(year_start)
+print(f'Time from the beginning of life {right_now - year_start}')
 
