d4c4cdcb7b99567da7d45263daa7381f84447ce6
AndrejTutka <tutkaandrejko@gmail.com>
2023-04-24 14:24:40 +0200

    Update sets exerises.py

--- List/sets exerises.py
+++ List/sets exerises.py
@@ -2,7 +2,7 @@ import random
 
 x=0
 tips = []
-while x < 100:
+while x < 1000000:
     tip=set()
     while len(tip) <6:
         tip.add(random.randint(1,49))
@@ -12,8 +12,6 @@ while x < 100:
 draw = set()
 while len(draw) <6:
         draw.add(random.randint(1,49))
-print(draw)
-print(tips)
 y=0
 place1=set()
 place2=set()
@@ -24,9 +22,9 @@ place6=set()
 place7=set()
     
 
-while y < 100:
+while y < 1000000:
     set3 = draw & tips[y]
-    print(set3, y+1)
+    
     y+=1
     if len(set3)==6:
         place1.add(y)
@@ -43,7 +41,7 @@ while y < 100:
     if len(set3)==0:
         place7.add(y)
         
-print(f'fisrt={place1},2nd={place2},3={place3},4={place4},5={place5},6={place6},7={place7}')
+print(f'WINNER={place1},2nd place{place2},3rd place={place3},4th place={place4},5th place={place5},6th place={place6},LOSER={place7}')
 
 
         
